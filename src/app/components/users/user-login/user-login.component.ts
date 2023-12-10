2d1b239ac5496e374661143decc8b46793b98ef2
Daniel_Dyn <102476758+DanieL-S22@users.noreply.github.com>
2023-12-10 18:13:02 -0400

    feat: user login front done

--- src/app/components/users/user-login/user-login.component.ts
+++ src/app/components/users/user-login/user-login.component.ts
@@ -4,10 +4,12 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css'],
 })
 export class UserLoginComponent {
-  constructor(private router: Router) { }
+  email: string = '';
+  password: string = '';
+  constructor(private router: Router) {}
   login(): void {
     // Lógica de autenticação, se necessário
     // Se a autenticação for bem-sucedida, navegue para a lista de receitas
