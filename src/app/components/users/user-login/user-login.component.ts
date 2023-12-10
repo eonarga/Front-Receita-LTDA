11b2ba43e7f553513fa8872813bc6f95288105b1
Daniel_Dyn <102476758+DanieL-S22@users.noreply.github.com>
2023-12-10 19:43:28 -0400

    fix: user auth done

--- src/app/components/users/user-login/user-login.component.ts
+++ src/app/components/users/user-login/user-login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { IUser } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-login',
@@ -9,10 +10,26 @@ import { Router } from '@angular/router';
 export class UserLoginComponent {
   email: string = '';
   password: string = '';
+
   constructor(private router: Router) {}
+
   login(): void {
-    // Lógica de autenticação, se necessário
-    // Se a autenticação for bem-sucedida, navegue para a lista de receitas
-    this.router.navigate(['/recipe-list']);
+    // Recupera os usuários salvos no localStorage
+    const storedUsersString: string | null = localStorage.getItem('users');
+    const users: IUser[] = storedUsersString
+      ? JSON.parse(storedUsersString)
+      : [];
+
+    // Verifica se há um usuário com as credenciais fornecidas
+    const user = users.find(
+      (u) => u.email === this.email && u.password === this.password
+    );
+
+    if (user) {
+      // Se a autenticação for bem-sucedida, navegue para a lista de receitas
+      this.router.navigate(['/recipe-list']);
+    } else {
+      console.log('Credenciais inválidas');
+    }
   }
 }
--- src/app/components/users/user-register/user-register.component.ts
+++ src/app/components/users/user-register/user-register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { IUser } from 'src/app/models/user';
+import { AuthGuardService } from 'src/app/services/auth/auth-guard.service';
 
 @Component({
   selector: 'app-user-register',
@@ -7,14 +9,41 @@ import { Router } from '@angular/router';
   styleUrls: ['./user-register.component.css'],
 })
 export class UserRegisterComponent {
-  name: string = '';
-  email: string = '';
-  password: string = '';
+  user: IUser = {
+    id: 0,
+    name: '',
+    email: '',
+    password: '',
+  };
   constructor(private router: Router) {}
   register(): void {
     // Lógica de registro, se necessário
-    // Se o registro for bem-sucedido, navegue para a lista de receitas
+    if (this.user.name && this.user.email && this.user.password) {
+      // Recupera os Usuários existentes no
+      const storedUsersString: string | null = localStorage.getItem('user');
+      const users: IUser[] = storedUsersString
+        ? JSON.parse(storedUsersString)
+        : [];
 
-    this.router.navigate(['/recipe-list']);
+      // Gera id para usuário
+      this.user.id = this.generateUserId(users);
+
+      //Adiciona o usuário ao array
+
+      users.push(this.user);
+
+      // Salva o array atualizado no localStorage
+      localStorage.setItem('users', JSON.stringify(users));
+
+      // Navega de volta para a tela de listagem de receitas
+      this.router.navigate(['/recipe-list']);
+    } else {
+      alert('Preencha todos os campos obrigatórios!');
+    }
+  }
+
+  private generateUserId(users: IUser[]): number {
+    //Gera um id único para a nova receita
+    return users.length > 0 ? Math.max(...users.map((user) => user.id)) + 1 : 1;
   }
 }
